7d75bea7bbe1d687b79dd2c47db296225aa0eb8a
Mazhr <ahmedmazhar20@gmail.com>
2021-01-18 02:25:07 +0200

    Create basic folder structures

---
+++ Run WSG.bat
@@ -0,0 +1,18 @@
+ECHO OFF
+
+Rem Create Directories
+
+mkdir "New Website"
+cd "New Website"
+mkdir resources
+mkdir playground
+cd resources
+mkdir js
+mkdir css
+mkdir lib
+mkdir doc
+mkdir img
+cd css
+mkdir fonts
+cd ..
+cd ..
